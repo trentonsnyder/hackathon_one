ec42ac7ed4ff91c1582d31c7e5b43e7d1441cf2d
ottjw <ottjw@yahoo.com>
2016-03-12 20:16:31 +0000

    got stuff to work

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -20,6 +20,7 @@ class ServicesController < ApplicationController
     else 
       flash[:alert] = "Something went Wrong"
       render :new
+    end
   end
 
   def edit
@@ -43,7 +44,7 @@ class ServicesController < ApplicationController
   private
 
   def vehicle
-    @vehicle = current_user.vehicles.find(params[:vehicle_id])
+   @vehicle = current_user.vehicles.find(params[:vehicle_id])
   end
 
   def service_params
